1a37f0de8e9248c619000d3961f90f94d9f4c898
Abdulloh <formygithubacc@gmail.com>
2020-08-16 04:26:42 +0500

    changes in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,9 +38,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Developed & Designed by{' '}
+          <a href="https://github.com/abon" target="_blank" rel="noopener noreferrer">
+            Abdullah Samadov
           </a>
         </p>
 
